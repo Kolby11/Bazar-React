7580f4b17f03f1e21d18d88fea47702ee4ea82be
Oaelc <96106083+Oaelc@users.noreply.github.com>
2023-02-02 20:44:40 +0100

    Main_stranka 20:44.02.02.2023

--- Main/Main.tsx
+++ Main/Main.tsx
@@ -5,26 +5,40 @@ export default function Main() {
   return (
     <div>
       <h1>Bazoš</h1>
-      <button type="button">Click Me!</button>
-      <table border="2" cellpadding="5" cellspacing="4">
+      <div className="topnav">
+      <label for="cars">Vyber značku auta:</label>
+
+<select name="cars" id="cars">
+  <option value="volvo">Volvo</option>
+  <option value="saab">Saab</option>
+  <option value="mercedes">Mercedes</option>
+  <option value="audi">Audi</option>
+</select>
+        <input type="text" placeholder="Search.."></input>
+      </div>
+      <table border="2" width="150%" cellpadding="10" cellspacing="5">
         <tr>
           <th>Fotka</th>
           <th>Cena</th>
           <th>Lokalita</th>
+          <th>Popis</th>
         </tr>
         <tr>
           <td></td>
           <td>&euro;</td>
           <td></td>
+          <td></td>
         </tr>
         <tr>
           <td></td>
           <td>&euro;</td>
+          <td></td>
           <td></td>
         </tr>
         <tr>
           <td></td>
           <td> &euro;</td>
+          <td></td>
           <td></td>
         </tr>
       </table>
