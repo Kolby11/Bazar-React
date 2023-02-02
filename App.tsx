0d5f3d50a603ab6dc4c9470acd02bdc6eba5ffe2
Oaelc <96106083+Oaelc@users.noreply.github.com>
2023-02-02 12:22:02 +0100

    1 file modified

--- App.tsx
+++ App.tsx
@@ -5,8 +5,7 @@ export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
-      <p>Start NEGER
-        to see some magic happen :)</p>
+      <p>Start editing to see some magic happen :)</p>
     </div>
   );
 }
