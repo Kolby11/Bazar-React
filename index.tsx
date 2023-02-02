26a6f4e571ea9ce69df8dc8fa0f1e8da5dadd828
Kolby11 <99053793+Kolby11@users.noreply.github.com>
2023-02-02 12:41:39 +0100

    Initial commit

--- index.tsx
+++ index.tsx
@@ -2,15 +2,9 @@ import * as React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import App from './App';
 import Profile from './Profile/Profile';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
-root.render(
-  <StrictMode>
-    <App />
-    <Profile />
-  </StrictMode>
-);
+root.render(<StrictMode></StrictMode>);
