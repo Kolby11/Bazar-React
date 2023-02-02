770a2c3d26a6b183b35d9ad302bd7205a2fa28bb
Kolby11 <99053793+Kolby11@users.noreply.github.com>
2023-02-02 13:15:16 +0100

    1 binaries uploaded

--- Profile/Profile.tsx
+++ Profile/Profile.tsx
@@ -4,7 +4,15 @@ import './style.css';
 export default function Profile() {
   return (
     <div>
-      <h1>Profile Page</h1>
+      <div className="top-bar">
+        {/* Pridat presmerovanie na hlavnu stranku*/}
+        <a href="index.html">Bazos</a>
+        <a href="settings.html">Nastavenia</a>
+      </div>
+      <div className="profile-info">
+        <img src="" alt="Profile Image"></img>
+        <p>Username</p>
+      </div>
     </div>
   );
 }
--- index.tsx
+++ index.tsx
@@ -3,8 +3,13 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import Profile from './Profile/Profile';
+import './style.css';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
-root.render(<StrictMode></StrictMode>);
+root.render(
+  <StrictMode>
+    <Profile />
+  </StrictMode>
+);
