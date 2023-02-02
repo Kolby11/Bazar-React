e97cc186be1a3045811ad36155fe9ba449ec547a
Oaelc <96106083+Oaelc@users.noreply.github.com>
2023-02-02 13:23:37 +0100

    Main stranka progress

--- Main/Main.tsx
+++ Main/Main.tsx
@@ -4,7 +4,30 @@ import './styles.css';
 export default function Main() {
   return (
     <div>
-      <h1>Main Page</h1>
+      <h1>Bazoš</h1>
+      <button type="button">Click Me!</button>
+      <table border="2" cellpadding="5" cellspacing="4">
+        <tr>
+          <th>Fotka</th>
+          <th>Cena</th>
+          <th>Lokalita</th>
+        </tr>
+        <tr>
+          <td></td>
+          <td>&euro;</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td>&euro;</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td> &euro;</td>
+          <td></td>
+        </tr>
+      </table>
     </div>
   );
 }
