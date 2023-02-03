4c65860c85808868bb5fa4e3450a7ea85e67174a
Oaelc <96106083+Oaelc@users.noreply.github.com>
2023-02-03 16:20:29 +0100

    Domovksa stránka 16:20.03.02.2023

--- Main/Main.tsx
+++ Main/Main.tsx
@@ -4,16 +4,36 @@ import './styles.css';
 export default function Main() {
   return (
     <div>
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+      ></link>
       <h1>Bazoš</h1>
+      <p>Icon buttons:</p>
+      <button class="btn">
+        <i class="fa fa-home"></i>
+      </button>
+      <button class="btn">
+        <i class="fa fa-bars"></i>
+      </button>
+      <button class="btn">
+        <i class="fa fa-trash"></i>
+      </button>
+      <button class="btn">
+        <i class="fa fa-close"></i>
+      </button>
+      <button class="btn">
+        <i class="fa fa-folder"></i>
+      </button>
       <div className="topnav">
-      <label for="cars">Vyber značku auta:</label>
+        <label for="cars">Vyber značku auta:</label>
 
-<select name="cars" id="cars">
-  <option value="volvo">Volvo</option>
-  <option value="saab">Saab</option>
-  <option value="mercedes">Mercedes</option>
-  <option value="audi">Audi</option>
-</select>
+        <select name="cars" id="cars">
+          <option value="volvo">Volvo</option>
+          <option value="saab">Saab</option>
+          <option value="mercedes">Mercedes</option>
+          <option value="audi">Audi</option>
+        </select>
         <input type="text" placeholder="Search.."></input>
       </div>
       <table border="2" width="150%" cellpadding="10" cellspacing="5">
