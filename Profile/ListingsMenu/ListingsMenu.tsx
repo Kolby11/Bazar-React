3ff5454351169b138a18fe1d09c37abfd17923b6
Kolby11 <99053793+Kolby11@users.noreply.github.com>
2023-02-03 17:18:44 +0100

    Added not working function

--- Profile/ListingsMenu/ListingsMenu.tsx
+++ Profile/ListingsMenu/ListingsMenu.tsx
@@ -2,14 +2,12 @@ import * as React from 'react';
 import './style.css';
 
 const maxListingsAmount: number = 7;
+const myListings: boolean = true;
 const listings = ['Car', 'Tshirt', 'Banana'];
-const ListingMenu = () => {
-  return (
-    <div>
-      <div className="listings-selection">
-        <button>Moje Inzeraty</button>
-        <button>Uložené inzeráty</button>
-      </div>
+const Listings = () => {
+  if (!myListings) {
+    //Saved Listings
+    return (
       <div>
         {listings.map((item) => (
           <div className="listing-display">
@@ -18,6 +16,29 @@ const ListingMenu = () => {
           </div>
         ))}
       </div>
+    );
+  } else {
+    return (
+      //My Listings
+      <div>
+        {listings.map((item) => (
+          <div className="listing-display">
+            <img alt="{item}"></img>
+            <p>{item}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
+};
+const ListingMenu = () => {
+  return (
+    <div>
+      <div className="listings-selection">
+        <button>Moje Inzeraty</button>
+        <button>Uložené inzeráty</button>
+      </div>
+      {Listings}
     </div>
   );
 };
