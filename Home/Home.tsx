cfc5133d201406564c3421331ce7a282e3c04a80
Kolby11 <99053793+Kolby11@users.noreply.github.com>
2023-02-04 11:37:49 +0100

    1 file modified, 2 added

---
+++ Home/Home.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Navbar from '../Settings/Navbar/Navbar';
+import './style.css';
+
+const Settings = () => {
+  return (
+    <div>
+      <p>HomePage</p>
+    </div>
+  );
+};
+export default Settings;
--- index.tsx
+++ index.tsx
@@ -2,15 +2,22 @@ import * as React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Home/Home';
 
 import Profile from './Profile/ProfilePage';
+import Settings from './Settings/Settings';
 import './style.css';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Profile />}></Route>
+        <Route path="/" element={<Home />}>
+          <Route path="profile" element={<Profile />}></Route>
+        </Route>
+        <Route path="/profile/" element={<Profile />}>
+          <Route path="settings" element={<Settings />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
