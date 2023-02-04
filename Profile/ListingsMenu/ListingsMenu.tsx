360fc809c279e2a0085a4c33f8c20da9b65830ea
Kolby11 <99053793+Kolby11@users.noreply.github.com>
2023-02-04 09:59:47 +0100

    Added displaying limited amout of listings

--- Profile/ListingsMenu/ListingsMenu.tsx
+++ Profile/ListingsMenu/ListingsMenu.tsx
@@ -1,44 +1,42 @@
 import * as React from 'react';
 import './style.css';
 
-const maxListingsAmount: number = 7;
-const myListings: boolean = true;
-const listings = ['Car', 'Tshirt', 'Banana'];
-const Listings = () => {
-  if (!myListings) {
-    //Saved Listings
-    return (
-      <div>
-        {listings.map((item) => (
-          <div className="listing-display">
-            <img alt="{item}"></img>
-            <p>{item}</p>
-          </div>
-        ))}
-      </div>
-    );
-  } else {
-    return (
-      //My Listings
-      <div>
-        {listings.map((item) => (
-          <div className="listing-display">
-            <img alt="{item}"></img>
-            <p>{item}</p>
-          </div>
-        ))}
-      </div>
-    );
-  }
-};
+const maxListingsAmount: number = 2;
+const myListingsArray = ['Car', 'Tshirt', 'Banana', 'Monitor'];
+const savedListingsArray = ['Cat', 'Trousers', 'Apple'];
+function Listings({ listings }: { listings: string[] }) {
+  return (
+    <div>
+      {listings.map((item, i) => {
+        if (i < maxListingsAmount) {
+          return (
+            <div className="listing-display">
+              <img alt="{item}" />
+              <p>
+                {item + ' '}
+                {i}
+              </p>
+            </div>
+          );
+        }
+        return;
+      })}
+    </div>
+  );
+}
 const ListingMenu = () => {
+  const [myListings, setMyListings] = React.useState(true);
   return (
     <div>
       <div className="listings-selection">
-        <button>Moje Inzeraty</button>
-        <button>Uložené inzeráty</button>
+        <button onClick={() => setMyListings(true)}>Moje Inzeraty</button>
+        <button onClick={() => setMyListings(false)}>Uložené inzeráty</button>
       </div>
-      {Listings}
+      {myListings ? (
+        <Listings listings={myListingsArray} />
+      ) : (
+        <Listings listings={savedListingsArray} />
+      )}
     </div>
   );
 };
