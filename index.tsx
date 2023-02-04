e2f42553f0d8e182c68b2f3a862cfb486a0376d2
Kolby11 <99053793+Kolby11@users.noreply.github.com>
2023-02-04 11:27:56 +0100

    Adding routing

--- index.tsx
+++ index.tsx
@@ -1,15 +1,20 @@
 import * as React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Profile from './Profile/ProfilePage';
 import './style.css';
 
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Profile />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
-root.render(
-  <StrictMode>
-    <Profile />
-  </StrictMode>
-);
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
