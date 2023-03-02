422e352d7fd6862fea248c4e1615efa2258eaa88
TomasBubenik1 <117894349+TomasBubenik1@users.noreply.github.com>
2023-03-02 12:45:33 +0100

    1 file modified

--- components/AddListing/AddListing.tsx
+++ components/AddListing/AddListing.tsx
@@ -6,18 +6,22 @@ import Form from 'react-bootstrap/Form';
 
 const AddListing = () => {
   return (
+    
     <Form>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
         <Form.Control type="email" placeholder="Enter email" />
         <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
+          So the buyer can contact you!
         </Form.Text>
       </Form.Group>
       
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Price:</Form.Label>
+        <Form.Control type="email" placeholder="Set the price of your item." />
+        <Form.Text className="text-muted">
+          So the buyer can contact you!
+        </Form.Text>
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicCheckbox">
         <Form.Check type="checkbox" label="Check me out" />
@@ -26,19 +30,7 @@ const AddListing = () => {
       <Button variant="primary" type="submit">
         Submit
       </Button>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-
-      <Form.Label htmlFor="exampleColorInput">Color picker</Form.Label>
-      <Form.Control
-        type="color"
-        id="exampleColorInput"
-        defaultValue="#563d7c"
-        title="Choose your color"
-      />
-
+  
 
 
     </Form>
