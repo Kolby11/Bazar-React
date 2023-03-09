0f386782838785e5af2db9a38f5b7aa9acb3f05f
TomasBubenik1 <117894349+TomasBubenik1@users.noreply.github.com>
2023-03-09 12:47:53 +0100

    93

--- components/AddListing/AddListing.tsx
+++ components/AddListing/AddListing.tsx
@@ -4,40 +4,74 @@ import Form from 'react-bootstrap/Form';
 
 
 
+let kategorie: string[] = [
+  'Zvieratá',
+  'Deti',
+  'Práca',
+  'Auto',
+  'Motocykle',
+  'Stroje',
+  'Záhrada',
+  'Mobily',
+  'PC',
+];
+
+
+
+
+
 const AddListing = () => {
   return (
+
+    <div className="d-flex justify-content-center">
+      <div className="align-bottom">
+        <div className="flex-row align-content-center">
+          <img src={'Images/profile_blank.png'} alt="Profile Image"></img>
+          <br />
+        
+        <form>
+        <br/>
+        <br/>
+          <label>Meno vášho inzeratu: </label>
+          <br/>
+          <input type="text" id="meno" placeholder="Meno inzeratu" />
+          <br/>
+          Zadajte cenu inzeratu:
+        <br/>
+      <input type="text" id="cena" placeholder="Cena" />
+          <br/>
+          Zadajte Lokalitu
+          <br/>
+          <input type="text" id="lokalita" placeholder="Lokalita" />
+        <br/>
+          <label>Kategoria:</label>
+          <br/>
+          <div>
+      <select name="cars" id="cars">
+        {kategorie.map((item) => {
+          return <option>{item}</option>;
+        })}
+      </select>
+       
+
+        <div id="SelectValue"></div>
+
+    </div>
     
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          So the buyer can contact you!
-        </Form.Text>
-      </Form.Group>
-      
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Price:</Form.Label>
-        <Form.Control type="email" placeholder="Set the price of your item." />
-        <Form.Text className="text-muted">
-          So the buyer can contact you!
-        </Form.Text>
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <br></br>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-  
-
-
-    </Form>
-  
-
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+        </form>
+      </div>
+      <button  type="submit" >Add Listing</button>
+    </div>
+  </div>
 
   );
 };
 
 export default AddListing;
+
+
+    
